1a00a63379bf9a86e2448b5b8072a2543b0598b6
GmalFari <gmalfari926@gmail.com>
2022-11-16 23:20:34 +0300

    a

--- project/settings.py
+++ project/settings.py
@@ -20,16 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a3p@)_ftbe89a-k5jt+kv$59js_$^$rzkh(1k6dk*!wmt)5rm2'
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-a3p@)_ftbe89a-k5jt+kv$59js_$^$rzkh(1k6dk*!wmt)5rm2') 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1" #1==True
 
 ALLOWED_HOSTS = []
-
-
+if not DEBUG:
+    ALLOWED_HOSTS +=[os.environ.get('DJANGO_ALLOWED_HOST')]
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
